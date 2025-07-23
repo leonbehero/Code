05e266c15da8bf530460300bbba3eed1415771bf
leonbehero <leonbehero@gmail.com>
2025-07-23 23:29:58 +0100

    Update run_code2prompt.bat

--- run_code2prompt.bat
+++ run_code2prompt.bat
@@ -1,18 +1,27 @@
 @echo off
 setlocal
 
-:: Ask for project path
-set /p PROJECT_PATH=Enter the full path to your project folder: 
-if "%PROJECT_PATH%"=="" (
-    echo ❌ No path entered. Exiting.
-    exit /b
-)
+echo Running Code2Prompt Tool
+echo ------------------------
 
-:: Ask for mode
-set /p MODE=Enter mode (explain/debug/clone) [default: explain]: 
-if "%MODE%"=="" set MODE=explain
+echo Select Mode:
+echo 1. explain
+echo 2. debug
+echo 3. clone
+echo 4. rewrite (old C# codebase)
+echo 5. rewrite-sample (Go example project)
 
-:: Run the program
-code2prompt.exe -path="%PROJECT_PATH%" -mode=%MODE%
+set /p mode=Enter mode number (1-5): 
+
+if "%mode%"=="1" set MODE=explain
+if "%mode%"=="2" set MODE=debug
+if "%mode%"=="3" set MODE=clone
+if "%mode%"=="4" set MODE=rewrite
+if "%mode%"=="5" set MODE=rewrite-sample
+
+set /p path=Enter the full path to your codebase folder:
+
+echo Running with mode %MODE% on %path%
+code2prompt.exe -mode=%MODE% -path="%path%"
+
 pause
-```
